b71e50e690d0ba5e4bf08309713faa23bc5e7527
kotableOrKisaki <super_misha05@mail.ru>
2022-08-27 23:23:57 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Напиши свое послание:\t");
+            string message = Console.ReadLine();
+
+            Console.Write("Сколько раз еще отправить?\t");
+            int countMessage = Convert.ToInt32(Console.ReadLine());
+
+            while (countMessage > 0)
+            {
+                countMessage--;
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
